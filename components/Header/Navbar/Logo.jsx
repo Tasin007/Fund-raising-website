a4613db314927f9c56e39a939a164ec52cedb23d
Tasin007 <mdtasinalam.mta@gmail.com>
2023-09-27 23:51:59 +0600

    routing completed and some assets modified

---
+++ components/Header/Navbar/Logo.jsx
@@ -0,0 +1,11 @@
+import logo from "./logo/Logo.png";
+
+const Logo = () => {
+    return (
+        <div>
+            <img className="w-[65%] lg:w-[100%]" src={logo} alt="" />
+        </div>
+    );
+}
+
+export default Logo;
